4b540cf094f8172b530067ebda6b5a83de25b6d8
Railson Martins <railsonmartins1970@gmail.com>
2025-06-30 13:52:46 -0300

    Add Inventory class

---
+++ cypress/pages/inventory/elements.js
@@ -0,0 +1,4 @@
+export const elements = {
+  addToCart: '[data-test="add-to-cart-sauce-labs-backpack"]',
+  removeToCart: '[data-test="remove-sauce-labs-backpack"]',
+};
--- cypress/pages/inventory/index.js
+++ cypress/pages/inventory/index.js
@@ -1,6 +1,15 @@
+import { elements as el } from "./elements";
 class Inventory {
   checkPageAccess() {
     cy.url().should("eq", "https://www.saucedemo.com/inventory.html");
+  }
+
+  addProduct() {
+    cy.get(el.addToCart).click();
+  }
+
+  removeProduct() {
+    cy.get(el.removeToCart).click();
   }
 }
 
